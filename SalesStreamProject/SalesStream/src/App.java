1fa4e1e5a76a56989ef37f082a901421d458ac9e
AlexMerlo1 <113566319+AlexMerlo1@users.noreply.github.com>
2023-11-06 11:15:26 -0500

    Setup all JavaFX Libraries to run

--- SalesStreamProject/SalesStream/src/App.java
+++ SalesStreamProject/SalesStream/src/App.java
@@ -1,5 +1,23 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        Scene scene = new Scene(new StackPane(l), 640, 480);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
 }
